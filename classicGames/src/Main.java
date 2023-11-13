ad07c28b10bbbfbd2c14588ab828ac800dfed167
Matt Brown <mattbrown@Matts-MacBook-Pro.local>
2023-11-13 12:25:32 -0500

    update1

--- classicGames/src/Main.java
+++ classicGames/src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("What's up!!");
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Classic Games!!!");
+        System.out.println();
+        System.out.println();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please select a Game to play:");
+        System.out.println("1) Rock Paper Scissors");
     }
 }
